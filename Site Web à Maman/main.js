1e5e51d7f2a17665d105a1ed8a24e2df1cac2ef1
Alaric Besharah <acb656@mail.usask.ca>
2019-06-07 20:28:05 -0600

    More work on practice section

--- Site Web à Maman/main.js
+++ Site Web à Maman/main.js
@@ -4,34 +4,31 @@ var coll = document.getElementsByClassName("collapsible");
 
 for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+
     this.classList.toggle("active");
+
     var content = this.nextElementSibling;
     var items = content.getElementsByClassName("tabcontent");
     var links = content.getElementsByClassName("tablinks");
+
+    // Hide everything in the content sections 
     for(var j = 1; j<items.length;j++){
       items[j].style.display = "none";
       links[j].className = links[j].className.replace(" active", "");
     }
+    
     items[0].style.display = "block";
+
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
     }
+    
   });
 }
 
-// var present = [
-//   ["verbe", "je",   "tu",    "il",   "nous",   "vous",  "ils"],
-
-//   ["aimer", "aime", "aimes", "aime", "aimons", "aimez", "aiment"],
-//   ["aller", "va", "vas", "va", "allons", "allez", "vont"],
-//   ["aimer", "aime", "aimes", "aime", "aimons", "aimez", "aiment"]
-// ];
-
-console.log(verbs[1]);
-
-function openTab(evt, cityName, node) {
+function openTab(evt, name, node) {
   
   var i;
   var tabcontent = node.parentNode.parentNode.getElementsByClassName("tabcontent");
@@ -44,6 +41,6 @@ function openTab(evt, cityName, node) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(name).style.display = "block";
   evt.currentTarget.className += " active";
 }
--- Site Web à Maman/verbs.js
+++ Site Web à Maman/verbs.js
@@ -1,24 +1,71 @@
-var vowel_list = 'aeiouAEIOU';
-
 var present = [
     ["present", "je",   "tu",    "il",   "nous",   "vous",  "ils"],
-  
     ["aimer", "aime", "aimes", "aime", "aimons", "aimez", "aiment"],
     ["aller", "va", "vas", "va", "allons", "allez", "vont"]
 ];
 
 var imparfait = [
-    ["imparfait", "je",   "tu",    "il",   "nous",   "vous",  "ils"],
-  
+    ["imparfait", "je", "tu",     "il",     "nous",    "vous",   "ils"],
     ["aimer", "aimais", "aimais", "aimait", "aimions", "aimiez", "aimaient"],
+    ["aller", "allais", "allais", "allait", "allions", "alliez", "allaient"],
     ["aller", "allais", "allais", "allait", "allions", "alliez", "allaient"]
 ];
 
-var verbs103 = [present];
-var verbs106 = [present, imparfait];
+var passéComposé = [
+    ["passé Composé", "je", "tu",     "il",     "nous",    "vous",   "ils"],
+    ["aimer", "aimais", "aimais", "aimait", "aimions", "aimiez", "aimaient"],
+    ["aller", "allais", "allais", "allait", "allions", "alliez", "allaient"],
+    ["aller", "allais", "allais", "allait", "allions", "alliez", "allaient"]
+];
 
+
+var verbs103 = [present];
+var verbs106 = [present, imparfait,passéComposé];
+var ready = true;
+var singleDescription = "Temporary FREN 103 verb practise description.";
+var multipleDescription = "Select all verbs tenses below which you want to practise";
+
+// Create the HTML elements for the verb quiz based on 
+// which list of verbs you pass as parameters
 function createVerbOptions(verbList){
-    for(var i = 0; i < verbList.length; i++){
-        
+
+    var desc = document.createElement("p");
+    desc.className = "description";
+
+    if(verbList.length > 1){
+
+        desc.innerHTML = multipleDescription;
+        document.getElementById("contentContainer").appendChild(desc);
+
+        var checkZone = document.getElementById("checkZone");
+        for(var i = 0; i < verbList.length; i++){
+            var div = document.createElement("div");
+            div.className = "checkRow";
+
+            var input = document.createElement("input");
+            input.type = "checkbox";
+
+            var para = document.createElement("p");
+            para.innerHTML = verbList[i][0][0];
+            para.className = "checkTag";
+
+            div.appendChild(input);
+            div.appendChild(para);
+
+            checkZone.appendChild(div);
+        }
+    } else {
+        desc.innerHTML = singleDescription;
+        document.getElementById("contentContainer").appendChild(desc);
     }
+    
 }
+
+// Returns true if first char of a string is a vowel, false otherwise
+function startsWithVowel(verb) {
+    var x = verb.charAt(0);
+    return ("aeiouAEIOU".indexOf(x) != -1); 
+}
+
+
+
